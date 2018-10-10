39a4d8c7e271dfa74532e02e6db740d5bb6886ed
Daryl-Conway-1995 <darylconway1995@gmail.com>
2018-10-10 16:45:06 +0100

    added rock paper scissors

--- src/main/scala/Rock_Paper_Scissors.scala
+++ src/main/scala/Rock_Paper_Scissors.scala
@@ -16,6 +16,7 @@ object Rock_Paper_Scissors {
     rock_Paper_Scissors("rock")
     rock_Paper_Scissors("paper")
     rock_Paper_Scissors("rock")
+    rock_Paper_Scissors("random words")
     rock_Paper_Scissors("scissors")
     rock_Paper_Scissors("paper")
     rock_Paper_Scissors("paper")
@@ -28,100 +29,98 @@ object Rock_Paper_Scissors {
   def rock_Paper_Scissors(user_Choice: String): Unit = {
     println(user_Choice)
     find_Results(user_Choice) match {
-      case "win" => {
+      case "win" =>
         win = win + 1
         println(s"you now have $win win(s)\n")
-      }
-      case "lose" => {
+
+      case "lose" =>
         lose = lose + 1
         println(s"you now have $lose lose(s)\n")
-      }
-      case "draw" => {
+
+      case "draw" =>
         draw = draw + 1
         println(s"now you have $draw draw(s)\n")
-      }
-      case "stop" => {
+
+      case "stop" =>
         println(s"Final results:\nWins: $win\nLoses: $lose\nDraws: $draw")
-      }
-      case _ => {
-        println("wrong input")
-      }
+
+      case _ =>
+        println("wrong input\n")
+
     }
   }
 
-
   def find_Results(user_Choice: String): String = user_Choice.toLowerCase match {
-    case "rock" => {
+    case "rock" =>
       userOptions(0) = userOptions.head + 1
       val computer_Choice = computer_Choose()
       computer_Choice match {
-        case "rock" => {
+        case "rock" =>
           println("you both picked rock it's a draw")
           best_move = "rock"
           "draw"
-        }
-        case "paper" => {
+
+        case "paper" =>
           println("computer picked paper you lose")
           best_move = "rock"
           "lose"
-        }
-        case "scissors" => {
+
+        case "scissors" =>
           println("computer picked scissors you win")
           best_move = "rock"
           "win"
-        }
+
       }
-    }
-    case "paper" => {
+
+    case "paper" =>
       userOptions(1) = userOptions(1) + 1
       val computer_Choice = computer_Choose()
       computer_Choice match {
-        case "rock" => {
+        case "rock" =>
           println("computer picked rock you win")
           best_move = "paper"
           "win"
-        }
-        case "paper" => {
+
+        case "paper" =>
           println("you both picked paper it's a draw")
           best_move = "paper"
           "draw"
-        }
-        case "scissors" => {
+
+        case "scissors" =>
           println("computer picked scissors you lose")
           best_move = "paper"
           "lose"
-        }
+
       }
-    }
-    case "scissors" => {
+
+    case "scissors" =>
       userOptions(2) = userOptions(2) + 1
       val computer_Choice = computer_Choose()
       computer_Choice match {
-        case "rock" => {
+        case "rock" =>
           println("computer picked rock you lose")
           best_move = "scissors"
           "lose"
-        }
-        case "paper" => {
+
+        case "paper" =>
           println("computer picked paper you win")
           best_move = "scissors"
           "win"
-        }
-        case "scissors" => {
+
+        case "scissors" =>
           println("you both picked scissors it's a draw")
           best_move = "scissors"
           "draw"
-        }
+
       }
-    }
-    case "stop" => {
+
+    case "stop" =>
       println("Thank you for playing")
       "stop"
-    }
-    case _ => {
-      println("Thank you for playing")
+
+    case _ =>
       "ignore"
-    }
+
   }
 
   def find_Best_Move(): Unit = {
