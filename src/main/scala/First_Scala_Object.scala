0eab4ad91e727b4ba48212a1345e76d63706c36b
Daryl-Conway-1995 <darylconway1995@gmail.com>
2018-10-10 11:41:21 +0100

    methods up to Functional 1 from scala exercises

--- src/main/scala/First_Scala_Object.scala
+++ src/main/scala/First_Scala_Object.scala
@@ -4,27 +4,52 @@ object First_Scala_Object {
 
   def main(args: Array[String]): Unit = {
     /*
+
+    //simple methods
     println("hello from the object\n")
     println(return_Input("Hello"))
     println(return_Input(234))
     println(chop_String("Hello",3))
     println(join_And_Change("Ha", "llo", 'a', 'e'))
+
+    //conditionals
     println(merge_Numbers(5,6,true))
     println(merge_Numbers(5,6,false))
     println(merge_Numbers(0,6,false))
+
+    //for loops
     iterate_String("Hi",3)
     iterate_Square_String("Hi",3)
     fizzBuzz("Fizz","Buzz",15)
+
+    //recursion
     recursion_Large_To_Small(5)
     recursion_Small_To_Large(5)
     string_Recursion("Hi", 3)
     recursion_FizzBuzz("Fizz","Buzz",20)
-    */
+
+    //reversing a list
     var myList = List(5, 7, 8, "String",5.7)
-    println(myList)
     println(reverse_List(myList))
+    reverse_Collection(Array(2,67))
+
+    //case statements
+    what_Month_Is(4)
+    what_Month_Is(12)
+    what_Month_Is(13)
+    println(what_Type_Is(List(4,4)))
+    println(what_Type_Is(4f))
+    println(what_Type_Is((4,4)))
+
+    get_Timezone_Ids()
+    */
+
+
+
 
   }
+
+  //region simple methods
 
   def return_Input(input_Value: Any): String = {
     input_Value.getClass.toString
@@ -35,8 +60,12 @@ object First_Scala_Object {
   }
 
   def join_And_Change(first_String: String, second_String: String, first_Char: Char, second_Char: Char): String = {
-    (first_String.concat(second_String)).replaceAll(first_Char.toString, second_Char.toString)
+    first_String.concat(second_String).replaceAll(first_Char.toString, second_Char.toString)
   }
+
+  //endregion simple methods
+
+  //region conditionals
 
   def merge_Numbers(first_Number: Int, second_Number: Int, is_Addition: Boolean): Int = {
     if (first_Number == 0)
@@ -44,10 +73,14 @@ object First_Scala_Object {
     else if (second_Number == 0)
       first_Number
     else if (is_Addition)
-      (first_Number + second_Number)
+      first_Number + second_Number
     else
-      (first_Number * second_Number)
+      first_Number * second_Number
   }
+
+  //endregion conditionals
+
+  //region for loops
 
   def iterate_String(input_String: String, iterations: Int): Unit = {
     for (a <- 1 to iterations)
@@ -73,6 +106,10 @@ object First_Scala_Object {
       else
         print(a + ", ")
   }
+
+  //endregion for loops
+
+  //region recursion
 
   def recursion_Large_To_Small(input_number: Int): Unit = {
     if (input_number > 0)
@@ -113,14 +150,79 @@ object First_Scala_Object {
     }
   }
 
+  //endregion
+
+  //region reverse lists
+
+  def reverse_Collection(x:Any):Unit={
+    x match {
+      case a:Array[_] =>
+        val a2 = a.reverse
+        a2.foreach(println)
+      case l:List[_] =>
+        val l2 = l.reverse
+        l2.foreach(println)
+      case t:(_,_) =>
+        val t2 = t.swap
+        println(t2)
+      case _ => println("not a valid option sorry")
+    }
+  }
 
   def reverse_List[A](input_List:List[A]):List[A]={
-    def remake_List[A](result: List[A],input_List:List[A]):List[A]={
+    //manual version of .reverse
+    def remake_List[_](result: List[A],input_List:List[A]):List[A]={
       input_List match {
         case Nil => result
-        case (element::list) => remake_List(element::result,list)
+        case element::list => remake_List(element::result,list)
       }
     }
     remake_List(Nil,input_List)
+  }
+
+  //endregion
+
+  //region case statements
+
+  def what_Month_Is(x: Int):Unit ={
+    x match{
+      case 1 => println("january")
+      case 2 => println("february")
+      case 3 => println("march")
+      case 4 => println("april")
+      case 5 => println("may")
+      case 6 => println("june")
+      case 7 => println("july")
+      case 8 => println("august")
+      case 9 => println("september")
+      case 10 => println("october")
+      case 11 => println("november")
+      case 12 => println("december")
+      case _ => println(s"Invalid number, month $x does not exist.")
+    }
+  }
+
+
+
+  def what_Type_Is(x: Any):String = x match{
+    case s:String => s + " is a String"
+    case i:Int => i + " is an Int"
+    case f:Float => f + " is a Float"
+    case l:List[_] => l + " is a List"
+    case _ => f" $x is a " + x.getClass
+  }
+
+  //endregion case statements
+
+  def get_Timezone_Ids():Unit={
+    //java.util.TimeZone.getAvailableIDs().foreach(println)
+    for(tmp<-java.util.TimeZone.getAvailableIDs()) {
+      val tmp2 = tmp split '/' takeRight 1
+      val tmp3 = tmp2 grouped 10
+      //val tmp3 = tmp2 sliding(10,10)
+      for(grouped_Elements <-tmp3)
+        for(subElement <-grouped_Elements)
+          println(subElement + "               "+ grouped_Elements.length)
+    }
   }
 }
