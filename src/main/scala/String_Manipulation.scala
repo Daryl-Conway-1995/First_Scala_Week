d5c4b50956222330394ec77b2a3a3467218203c9
Daryl-Conway-1995 <darylconway1995@gmail.com>
2018-10-11 16:50:39 +0100

    added string differences and finding common letters

--- src/main/scala/String_Manipulation.scala
+++ src/main/scala/String_Manipulation.scala
@@ -5,6 +5,9 @@ object String_Manipulation {
     //input1 = love of long words, input2 = fear of long words
     println(results)
 
+    find_Common_Sequences("watch your step", "look out below")
+
+
   }
 
 
@@ -19,9 +22,15 @@ object String_Manipulation {
     subStrings1.intersect(subStrings2).maxBy(_.length)
   }
 
-  def find_Common_Letters(input1: String, input2: String): String = {
-    //to be made
-    ""
+  def find_Common_Sequences(input1: String, input2: String): Unit = {
+    val subStrings1 = get_All_SubStrings(input1)
+    val subStrings2 = get_All_SubStrings(input2)
+
+    val common_letters = subStrings1.intersect(subStrings2).mkString("")
+    val missing_Letters1 = input1.diff(common_letters)
+    val missing_Letters2 = input2.diff(common_letters)
+    println("The matching characters between \"" + input1 + "\" and \"" + input2 + "\" are \"" + common_letters + "\". The characters that are " +
+      "not shared are: \"" + missing_Letters1 + "\" for \"" + input1 + "\" and \"" + missing_Letters2 + "\" for \"" + input2 + "\"")
   }
 
 }
