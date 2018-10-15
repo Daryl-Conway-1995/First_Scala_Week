1aa3ee4ac5a2e8a0172a169a98b25c1a649752bf
Daryl-Conway-1995 <darylconway1995@gmail.com>
2018-10-15 16:26:49 +0100

    added universal methods

---
+++ src/main/scala/Fibinacci/TestFibonacci.scala
@@ -0,0 +1,18 @@
+package Fibinacci
+
+object TestFibonacci {
+
+  def main(args: Array[String]): Unit = {
+    for(i<-0 to 15)println(fibonacci(i))
+  }
+
+  def fibonacci(inputNumber:Int):Int = {
+    @annotation.tailrec
+    def fibonacciTail(position: Int, firstNumber: Int, secondNumber: Int): Int = position match {
+      case 0 => firstNumber
+      case _ => fibonacciTail(position - 1, secondNumber, firstNumber + secondNumber)
+    }
+    fibonacciTail(inputNumber,0,1)
+  }
+
+}
